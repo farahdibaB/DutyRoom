a5821e6e0c48348005b4a6f288f81bb4fe392296
Farah <farah-bayat@hotmail.com>
2023-09-21 17:08:36 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-def get_database_location():
-    return os.environ.get("DATABASE_LOCATION", "/")
+# def get_database_location():
+#     return os.environ.get("DATABASE_LOCATION", "/")
 
 def get_port():
     return os.environ.get("PORT", 3000)
